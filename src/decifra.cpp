65ab500149d46dfeadaf0b325b2fa4c07cda6875
Valmir Correa <valmir.correa@ufrn.edu.br>
2018-09-17 09:19:40 -0300

    gerando as duas chaves k2 e k2

--- src/decifra.cpp
+++ src/decifra.cpp
@@ -73,6 +73,7 @@ void escrita (string msg, string nome_arq) {
     arquivo.close();
 }
 
+// rotação para chave 1
 string rotacao_esq(char *chave_per) {
 
     char chave_rotacionada[10];
@@ -92,13 +93,13 @@ string rotacao_esq(char *chave_per) {
 }
 
 /**
-* @brief    Cifra a mensagem recebida
+* @brief    CGera chave k1
 * @param    msg Mensagem a ser cifrada
 * @param    data Data
 * @return   Mensagem cifrada
 */
-string decifrar (bitset<10> chave_bit) {
-
+string gerar_sub_chave_k1 (bitset<10> chave_bit) {
+     
     // Base para Permutações
     int P10[10] = {3,5,2,7,4,10,1,9,8,6};
     int P8[8] = {6,3,7,4,8,5,10,9};
@@ -117,7 +118,7 @@ string decifrar (bitset<10> chave_bit) {
     // Separação e rotação
     string  chave_rotacionada = rotacao_esq(chave_per);   
 
-    cout << endl << "Chave rotacionada: " << chave_rotacionada << endl;
+    cout << endl << "Rotação a esquerda LS-1: " << chave_rotacionada << endl;
 
     // Permutação SW (P8)
     bitset<10> chave_per_p8 (chave_rotacionada);
@@ -131,6 +132,110 @@ string decifrar (bitset<10> chave_bit) {
     cout << "sub_chave_k1 : " << sub_chave_k1 << endl;
    
     return sub_chave_k1 ;
+}
+
+// Rotação para a chave 2
+string rotacao_esq_2(char *chave_per) {
+
+    char chave_rotacionada[10];
+
+    cout << endl << "teste:   ";
+    for ( int ii = 0; ii < 10; ii++) {
+        cout << chave_per[ii];
+    }
+    string chave_completa = "";
+    chave_completa += chave_per[2];
+    chave_completa += chave_per[3];
+    chave_completa += chave_per[4];
+    chave_completa += chave_per[0];
+    chave_completa += chave_per[1];
+    chave_completa += chave_per[7];
+    chave_completa += chave_per[8];
+    chave_completa += chave_per[9];
+    chave_completa += chave_per[5];
+    chave_completa += chave_per[6];
+
+    return chave_completa;
+}
+
+/**
+* @brief    CGera chave k1
+* @param    msg Mensagem a ser cifrada
+* @param    data Data
+* @return   Mensagem cifrada
+*/
+string gerar_sub_chave_k2 (bitset<10> chave_bit) {
+     
+    // Base para Permutações
+    int P10[10] = {3,5,2,7,4,10,1,9,8,6};
+    int P8[8] = {6,3,7,4,8,5,10,9};
+    char chave_per[10];
+
+    // Permutação inicial com P10
+    cout << "Permutação inicial (P10): ";
+    for (int ii = 0; ii < 10; ii++) {
+
+        int indice_per = P10[ii]-1;
+        chave_per[ii]  = chave_bit.to_string()[indice_per];
+        cout << chave_per[ii];
+        
+    }
+
+    // Separação e rotação
+    string  LS_1 = rotacao_esq(chave_per);
+
+    // ============================ a partir daqui que muda
+    char resultado_LS_1[10];
+    for (int ii = 0; ii < 10; ii++) {
+        resultado_LS_1[ii] = LS_1[ii];
+    }
+
+    // Separação e rotação
+    string  chave_rotacionada = rotacao_esq_2(resultado_LS_1);   
+    cout << endl << "Rotação a esquerda LS-2 duas posições: " << chave_rotacionada << endl;
+
+    // Permutação SW (P8)
+    bitset<10> chave_per_p8 (chave_rotacionada);
+    string sub_chave_k2 = "";
+    for (int ii = 0; ii < 8; ii++) {
+
+        int indice_per = P8[ii]-1;
+        sub_chave_k2  += chave_per_p8.to_string()[indice_per];   
+    }
+
+    cout << "sub_chave_k2 : " << sub_chave_k2 << endl;
+   
+    return sub_chave_k2 ;
+}
+
+bitset<8> funcao_complexa (/*bitset<8> texto_bit,*/ bitset<10> k1, bitset<10> k2 ) {
+
+    int IP[8]       = {2,6,3,1,4,8,5,7};
+    int EP[8]       = {4,1,2,3,2,3,4,1};
+    int S0[4][4]    = {{1,0,3,2}, {3,2,1,0}, {0,2,1,3}, {3,1,3,2}};
+    int S1[4][4]    = {{1,1,2,3}, {2,0,1,3}, {3,0,1,0}, {2,1,0,3}};
+    int P4[4]       = {2,4,3,1};
+    int IP_1[8]     = {4,1,3,5,7,2,8,6};
+
+}
+
+/**
+* @brief    Cifra a mensagem recebida
+* @param    msg Mensagem a ser cifrada
+* @param    data Data
+* @return   Mensagem cifrada
+*/
+string decifrar (bitset<10> chave_bit) {
+
+   string k1_str = gerar_sub_chave_k1 (chave_bit);
+   string k2_str = gerar_sub_chave_k2 (chave_bit);
+
+    bitset<10> k1 (k1_str);
+     bitset<10> k2 (k2_str);
+
+   funcao_complexa (k1, k2);
+
+   return "teste";
 }
 
 
